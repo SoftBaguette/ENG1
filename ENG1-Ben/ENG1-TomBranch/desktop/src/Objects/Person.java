d20af0f0585bbf7d5603c56dd2efdc73a01ffa59
arrighiii <117850037+arrighiii@users.noreply.github.com>
2022-12-16 13:53:14 +0000

    Add files via upload
    Forgot to remove the old BenBranch folder, Ben should now just contain an updated TomBranch

---
+++ ENG1-Ben/ENG1-TomBranch/desktop/src/Objects/Person.java
@@ -0,0 +1,10 @@
+package Objects;
+
+public class Person extends GameObject {
+
+    public Person(int x, int y, int width, int height) {
+        super(x, y, width, height);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
