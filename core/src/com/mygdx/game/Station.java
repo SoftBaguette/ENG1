692643354227d2ac9d5ef7a09f125d55e5884fc9
Piebee007 <tom.feilden@btinternet.com>
2023-01-19 11:36:26 +0000

    Fixes to do

--- core/src/com/mygdx/game/Station.java
+++ core/src/com/mygdx/game/Station.java
@@ -136,11 +136,14 @@ public class Station extends GameObject{
                     if (chef.stack.isEmpty() != true){
                         if (chef.stack.peak().name == item.name && chef.stack.peak().status == item.status){
                             items_on_station[++pointer] = chef.stack.pop();
+                            if (pointer == 10){
+                                pointer = 0;
+                            }
                             System.out.println("\n" + item.name + " added to assemly station");
                         }
                     }
                 }
-
+                // TODO FIX this section
                 // Check if all the items on the assembly station
                 // are right for the recipe
                 int items_ready = 0;
