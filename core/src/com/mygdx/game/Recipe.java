8cb73b768cb97199bd8a3bef0443984e158455c8
Piebee007 <74003575+Piebee007@users.noreply.github.com>
2023-01-24 00:19:39 +0000

    Update Recipe.java

--- core/src/com/mygdx/game/Recipe.java
+++ core/src/com/mygdx/game/Recipe.java
@@ -1,5 +1,10 @@
 package com.mygdx.game;
 
+/*
+    Each recipe has a name and then a list of ingredients it needs to make it
+    This is used for the assembly station to know what items are needed to assemble the food item
+*/
+
 public class Recipe {
     String recipe_name;
     Item[] ingredients;
